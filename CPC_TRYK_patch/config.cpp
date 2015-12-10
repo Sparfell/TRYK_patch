dbb53c68972d940b80ab78ccc7a276025bbafa18
Sparfell <arsparfell19@gmail.com>
2015-12-10 20:46:41 +0100

    Base structure commit
    posé de la structure de l'addon

---
+++ CPC_TRYK_patch/config.cpp
@@ -0,0 +1,38 @@
+/*
+class CfgMods
+{
+	class Mod_Base;
+	class cpc_nam : Mod_Base
+	{
+		picture = "\cpc_nam_core\data\cpc_nam_logo.paa";
+		logo = "\cpc_nam_core\data\cpc_nam_logo.paa";
+		logoOver = "\cpc_nam_core\data\cpc_nam_logo.paa";
+		logoSmall = "\cpc_nam_core\data\cpc_nam_logo.paa";
+		tooltip = "CPC NAM";
+		tooltipOwned = "CPC NAM";
+		action = "";
+		dlcColor[] = {0.23,0.39,0.30,1};
+		overview = "These Boots are made for walking.";
+		hideName = 1;
+		hidePicture = 0;
+		name = "CPC NAM";
+		dir = "CPC NAM";
+	};
+};
+*/
+
+class CfgPatches
+{
+	class CPC_TRYK_patch
+	{
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+	};
+};
+
+
+#include "CfgMagazines.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgVehicles.hpp"
